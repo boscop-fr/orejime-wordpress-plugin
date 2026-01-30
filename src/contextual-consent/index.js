69fbb92452154cc7f3a55c5469a262b9acf4e1ce
Félix Girault <felix.girault@gmail.com>
2026-01-30 12:12:46 +0100

    Added transforms to the contextual consent block
    This allows wrapping blocks with contextual consent easily.

--- src/contextual-consent/index.js
+++ src/contextual-consent/index.js
@@ -1,4 +1,4 @@
-import { registerBlockType } from '@wordpress/blocks';
+import { createBlock, registerBlockType } from '@wordpress/blocks';
 import metadata from './block.json';
 import edit from './edit';
 import save from './save';
@@ -6,4 +6,28 @@ import save from './save';
 registerBlockType( metadata.name, {
 	edit,
 	save,
+	transforms: {
+		// @see core/group transforms
+		from: [
+			{
+				type: 'block',
+				isMultiBlock: true,
+				blocks: [ '*' ],
+				__experimentalConvert( blocks ) {
+					const innerBlocks = blocks.map( ( block ) =>
+						createBlock(
+							block.name,
+							block.attributes,
+							block.innerBlocks
+						)
+					);
+
+					return createBlock( metadata.name, {}, innerBlocks );
+				},
+			},
+		],
+		// @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-transforms/#ungroup-blocks
+		ungroup: ( attributes, innerBlocks ) =>
+			innerBlocks.flatMap( ( innerBlock ) => innerBlock.innerBlocks ),
+	},
 } );
